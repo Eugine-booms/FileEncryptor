d64f2b34b832c401449c48b45e870c6a17c82b7b
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-01 12:09:42 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FileEncryptor.ViewModels.Base
+{
+   internal abstract class  ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+      protected void OnPropertyChange([CallerMemberName] string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+       protected virtual bool Set<T>(ref T fild, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(fild, value)) return false;
+            fild = value;
+            OnPropertyChange(PropertyName);
+            return true;
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileEncryptor.ViewModels
+{
+   internal class MainWindowViewModel : Base.ViewModelBase
+    {
+
+
+        #region  string Title Заголовок Окна
+        ///<summary> Заголовок Окна
+        private string _Title;
+        ///<summary> Заголовок Окна
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value, nameof(Title));
+        }
+        #endregion
+
+
+
+
+
+    }
+}
+
