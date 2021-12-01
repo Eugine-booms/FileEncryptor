94da1092d3704d3d217cf588c1999cb8aa9c7013
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-01 13:02:31 +0300

    Класс программ

---
+++ Programm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace FileEncryptor
+{
+   internal class Programm
+    {
+        [STAThread]
+        
+        public static void Main(string [] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
