d784f01c81c81932259bf818ed901d7452b02c60
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-01 15:27:50 +0300

    Диалог с пользователем

--- Services/Interfaces/IUserDialog.cs
+++ Services/Interfaces/IUserDialog.cs
@@ -6,5 +6,7 @@ namespace FileEncryptor.Services.Interfaces
 {
     interface IUserDialog
     {
+        bool OpenFile(string title, out string selectdFile, string filter = "Все файлы (*.*)|*.*");
+        bool OpenFiles(string title, out IEnumerable <string> selectdFiles, string filter = "Все файлы (*.*)|*.*");
     }
 }
--- Services/UserDialogService.cs
+++ Services/UserDialogService.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using FileEncryptor.Services.Interfaces;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FileEncryptor.Services
 {
-    class UserDialogService : Interfaces.IUserDialog
+    class UserDialogService : IUserDialog
     {
+        public bool OpenFile(string title, out string selectdFile, string filter = "Все файлы (*.*)|*.*")
+        {
+            var file_dialog = new OpenFileDialog()
+            {
+                Title = title,
+                Filter = filter
+            };
+            if (file_dialog.ShowDialog() !=true)
+            {
+                selectdFile = null;
+                return false;
+            }
+            selectdFile = file_dialog.FileName;
+            return true;
+        }
+
+        public bool OpenFiles(string title, out IEnumerable<string> selectdFiles, string filter = "Все файлы (*.*)|*.*")
+        {
+            var file_dialog = new OpenFileDialog()
+            {
+                Title = title,
+                Filter = filter
+            };
+            if (file_dialog.ShowDialog() != true)
+            {
+                selectdFiles = Enumerable.Empty<string>();
+                return false;
+            }
+            selectdFiles = file_dialog.FileNames;
+            return true;
+        }
     }
 }
